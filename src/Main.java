594470db8fd27a9d1a3e0a940e7766a902a3a1eb
jrodriguezcode <jr698751@gmail.com>
2025-02-03 13:27:50 -0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int x; // declaration
+		x = 123; // assignment
+		
+		int y = 456; // initialization
+		
+		System.out.println(x);
+		
+		System.out.println("My number is: " + y);
+		
+		long z = 1234567890123456609L;
+		
+		byte a = 100;
+		
+		double b = 123.01;
+		
+		float c = 3.14f;
+		
+		boolean d = true;
+		
+		char symbol = '@';
+		
+		String name = "Cloud";
+	}
+
+}
